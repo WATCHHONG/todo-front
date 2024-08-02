3fafb6df368c3619671aa64bc56cb359082831bd
watchhong <wlghd0101@naver.com>
2024-08-02 08:40:57 +0000

    frontend deployment

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {   //로컬로 접속시 연결 설정
   backendHost = "http://localhost:8080";
 }else {                                    // 로컬이 아닌 경우에 연결 설정
-  backendHost = "http://dev.watchhong.kro.kr";
+  backendHost = "https://dev.watchhong.kro.kr";
 }
 
 export const API_BASE_URL = `${backendHost}`;
